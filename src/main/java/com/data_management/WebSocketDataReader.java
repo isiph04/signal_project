dff5937c10b4d51057e2041e6c265873be78f2b4
Ilaria Siphouna <i.siphouna@student.maastrichtuniversity.nl>
2024-05-17 17:48:47 +0200

    Websocket Classes Implementation

--- src/main/java/com/data_management/WebSocketDataReader.java
+++ src/main/java/com/data_management/WebSocketDataReader.java
@@ -1,4 +1,57 @@
 package com.data_management;
 
-public class WebSocketDataReader {
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class WebSocketDataReader extends dataWebSocketClient implements DataReader
+{
+    //files are read locally
+    // websocket is connected to the server
+    private DataStorage dataStorage;
+
+    public WebSocketDataReader(URI serverUri, DataStorage dataStorage)
+    {
+        super(serverUri);
+        this.dataStorage = dataStorage;
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata)
+    {
+        System.out.println("Connected was opened");
+    }
+
+    @Override
+    public void onMessage(String message)
+    {
+        String[] dataCollected = message.split(",");
+        int patientId = Integer.parseInt(dataCollected[0].trim());
+        long timestamp = Long.parseLong(dataCollected[1].trim());
+        String label = dataCollected[2].trim();
+        double dataValue = Double.parseDouble(dataCollected[3].trim());
+        dataStorage.addPatientData(patientId, dataValue, label, timestamp);
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote)
+    {
+        System.out.println("Connection was closed");
+    }
+
+    @Override
+    public void onError(Exception ex)
+    {
+        ex.printStackTrace();
+    }
+
+    @Override
+    public void readData(DataStorage dataStorage) throws IOException {}
+
+    @Override
+    public void connect(URI serverUri) throws IOException
+    {
+        this.connect();
+    }
 }
--- src/main/java/com/data_management/dataWebSocketClient.java
+++ src/main/java/com/data_management/dataWebSocketClient.java
@@ -1,4 +1,63 @@
 package com.data_management;
 
-public class dataWebSocketClient {
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class dataWebSocketClient extends WebSocketClient
+{
+    public dataWebSocketClient(URI serverUri)
+    {
+        super(serverUri);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata)
+    {
+        System.out.println("Connected was opened");
+    }
+
+    @Override
+    public void onMessage(String message)
+    {
+        System.out.println("Message received");
+        String[] dataCollected = message.split(",");
+        if (dataCollected.length != 4)
+        {
+            System.err.println("Message has invalid format");
+        }
+        try
+        {
+            int patientId = Integer.parseInt(dataCollected[0].trim());
+            long timestamp = Long.parseLong(dataCollected[1].trim());
+            String label = dataCollected[2].trim();
+            double dataValue = Double.parseDouble(dataCollected[3].trim());
+        }
+        catch (NumberFormatException e)
+        {
+            System.err.println("Error when parsing the message");
+        }
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote)
+    {
+        System.out.println("Connection was closed");
+    }
+
+    @Override
+    public void onError(Exception ex)
+    {
+        System.err.println("An error occurred: " + ex.getMessage());
+    }
+
+    public static void main(String[] args) throws URISyntaxException
+    {
+       WebSocketClient client = new dataWebSocketClient(new URI ("ws://localhost:8887"));
+       client.connect();
+    }
+
 }
