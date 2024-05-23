ec285680ee522c124e1c12fb1c94d1e9991ea77c
Ilaria Siphouna <i.siphouna@student.maastrichtuniversity.nl>
2024-05-23 11:39:03 +0200

    websocket reader handles errors

--- src/main/java/com/data_management/WebSocketDataReader.java
+++ src/main/java/com/data_management/WebSocketDataReader.java
@@ -26,12 +26,23 @@ public class WebSocketDataReader extends dataWebSocketClient implements DataRead
     @Override
     public void onMessage(String message)
     {
-        String[] dataCollected = message.split(",");
-        int patientId = Integer.parseInt(dataCollected[0].trim());
-        long timestamp = Long.parseLong(dataCollected[1].trim());
-        String label = dataCollected[2].trim();
-        double dataValue = Double.parseDouble(dataCollected[3].trim());
-        dataStorage.addPatientData(patientId, dataValue, label, timestamp);
+        try {
+            String[] dataCollected = message.split(",");
+            if (dataCollected.length != 4) {
+                throw new IllegalArgumentException("Message has invalid format");
+            }
+            int patientId = Integer.parseInt(dataCollected[0].trim());
+            long timestamp = Long.parseLong(dataCollected[1].trim());
+            String label = dataCollected[2].trim();
+            double dataValue = Double.parseDouble(dataCollected[3].trim());
+            dataStorage.addPatientData(patientId, dataValue, label, timestamp);
+        } catch (NumberFormatException e) {
+            System.err.println("Error when parsing the message: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Invalid message format: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("An error occurred while processing this message: " + e.getMessage());
+        }
     }
 
     @Override
